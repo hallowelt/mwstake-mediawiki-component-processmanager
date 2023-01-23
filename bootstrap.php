f19d63c7a512f7cbd49c83d705167c2246202d11
Hamish Slater <hamishslater@hotmail.com>
2023-01-23 14:07:34 +0100

    Bump composer/installers version to 2
    ERM29065
    
    [main]

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ if ( defined( 'MWSTAKE_MEDIAWIKI_COMPONENT_PROCESSMANAGER_VERSION' ) ) {
 	return;
 }
 
-define( 'MWSTAKE_MEDIAWIKI_COMPONENT_PROCESSMANAGER_VERSION', '2.0.1' );
+define( 'MWSTAKE_MEDIAWIKI_COMPONENT_PROCESSMANAGER_VERSION', '2.0.2' );
 
 Bootstrapper::getInstance()
 	->register( 'processmanager', function () {
